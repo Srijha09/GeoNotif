7192a05c95bec71844984df0a5f06eede06f717f
eshwarhs99 <hebburshivakumar.e@northeastern.edu>
2023-02-13 17:19:02 -0500

    Add buttons in Mani Activity

---
+++ app/src/main/java/edu/northeastern/numadsp23_team20/AtYourService.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numadsp23_team20;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtYourService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_at_your_service);
+    }
+}
