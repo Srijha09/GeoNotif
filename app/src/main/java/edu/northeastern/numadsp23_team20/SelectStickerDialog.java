c30ba16826c892a57de9f9af437a8871a95d53e1
Rahul Suresh <rahs98@gmail.com>
2023-02-28 01:24:03 -0500

    Sticker chat

--- app/src/main/java/edu/northeastern/numadsp23_team20/SelectStickerDialog.java
+++ app/src/main/java/edu/northeastern/numadsp23_team20/SelectStickerDialog.java
@@ -2,6 +2,7 @@ package edu.northeastern.numadsp23_team20;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,16 +10,62 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class SelectStickerDialog extends AppCompatDialogFragment {
+
+    List<String> stickers;
+    AlertDialog.Builder builder;
+    SelectStickerListener selectStickerListener;
 
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Alert Dialog");
-        builder.setMessage("Alert Dialog inside DialogFragment");
+        this.stickers = Arrays.asList(
+                "pikachu", "mewtwo", "charmander", "squirtle", "groudon", "jolteon"
+        );
+        this.builder = new AlertDialog.Builder(getActivity());
+        this.builder.setTitle("Select a sticker");
         View view = getActivity().getLayoutInflater().inflate(R.layout.select_sticker_dialog, null);
-        builder.setView(view);
+        for (String sticker: this.stickers) {
+            view.findViewById(this.getId(sticker)).setOnClickListener(v -> {
+                selectStickerListener.applyInformation(sticker, v.getId());
+            });
+        }
+        this.builder.setView(view);
         return builder.create();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        try {
+            this.selectStickerListener = (SelectStickerListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException("onAttach error: " + context.toString());
+        }
+    }
+
+    private int getId(String sticker) {
+        switch (sticker) {
+            case "pikachu":
+                return R.id.pikachu;
+            case "mewtwo":
+                return R.id.mewtwo;
+            case "charmander":
+                return R.id.charmander;
+            case "squirtle":
+                return R.id.squirtle;
+            case "groudon":
+                return R.id.groudon;
+            case "jolteon":
+                return R.id.jolteon;
+        }
+        return 0;
+    }
+
+    public interface SelectStickerListener {
+        void applyInformation(String selectedStickerName, int selectedStickerId);
     }
 }
--- app/src/main/java/edu/northeastern/numadsp23_team20/StickItToEm.java
+++ app/src/main/java/edu/northeastern/numadsp23_team20/StickItToEm.java
@@ -1,20 +1,71 @@
 package edu.northeastern.numadsp23_team20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.constraintlayout.widget.Constraints;
 
 import android.os.Bundle;
+import android.util.LayoutDirection;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
-public class StickItToEm extends AppCompatActivity {
+public class StickItToEm extends AppCompatActivity implements SelectStickerDialog.SelectStickerListener {
+
+    SelectStickerDialog selectStickerDialog;
+    LinearLayout linearChatLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stick_it_to_em);
+        this.linearChatLayout = findViewById(R.id.LinearChatLayout);
     }
 
     public void onSendStickerButtonClick(View view) {
-        SelectStickerDialog selectStickerDialog = new SelectStickerDialog();
-        selectStickerDialog.show(getSupportFragmentManager(), "SelectStickerDialog");
+        this.selectStickerDialog = new SelectStickerDialog();
+        this.selectStickerDialog.show(getSupportFragmentManager(), "SelectStickerDialog");
+    }
+
+    @Override
+    public void applyInformation(String selectedStickerName, int selectedStickerId) {
+        this.selectStickerDialog.dismiss();
+        LinearLayout linearLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams linearLayoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, 200
+        );
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(this.getDrawable(selectedStickerName));
+        LinearLayout.LayoutParams imageViewParams = new LinearLayout.LayoutParams(
+                200, 200
+        );
+        imageView.setLayoutParams(imageViewParams);
+        linearLayout.addView(imageView);
+        linearLayout.setGravity(Gravity.END);
+        linearLayout.setLayoutParams(linearLayoutParams);
+        this.linearChatLayout.addView(linearLayout);
+    }
+
+    private int getDrawable(String sticker) {
+        switch (sticker) {
+            case "pikachu":
+                return R.drawable.pikachu;
+            case "mewtwo":
+                return R.drawable.mewtwo;
+            case "charmander":
+                return R.drawable.charmander;
+            case "squirtle":
+                return R.drawable.squirtle;
+            case "groudon":
+                return R.drawable.groudon;
+            case "jolteon":
+                return R.drawable.jolteon;
+        }
+        return 0;
     }
 }
