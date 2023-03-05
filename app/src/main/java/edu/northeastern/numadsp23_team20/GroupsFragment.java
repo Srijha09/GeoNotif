ca555938c9715560ee420bf030a7321f217e152c
Rahul Suresh <rahs98@gmail.com>
2023-03-04 19:19:17 -0500

    Home page with navigation

---
+++ app/src/main/java/edu/northeastern/numadsp23_team20/GroupsFragment.java
@@ -0,0 +1,22 @@
+package edu.northeastern.numadsp23_team20;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class GroupsFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_groups, container, false);
+    }
+}
--- app/src/main/java/edu/northeastern/numadsp23_team20/HomePage.java
+++ app/src/main/java/edu/northeastern/numadsp23_team20/HomePage.java
@@ -1,6 +1,7 @@
 package edu.northeastern.numadsp23_team20;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -8,14 +9,39 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomePage extends AppCompatActivity {
 
+    private Fragment tasksFragment;
+    private Fragment groupsFragment;
+    private Fragment friendsFragment;
+    private Fragment settingsFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        this.tasksFragment = new TasksFragment();
+        this.groupsFragment = new GroupsFragment();
+        this.friendsFragment = new FriendsFragment();
+        this.settingsFragment = new SettingsFragment();
         BottomNavigationView bottomNavigationMenu = findViewById(R.id.BottomNavigationMenu);
+        this.setFragment(this.tasksFragment);
         bottomNavigationMenu.setOnItemSelectedListener(item -> {
-            System.out.println(item.getTitle());
+            this.setFragment(this.getFragment(item.getTitle().toString()));
             return true;
         });
+    }
+
+    private Fragment getFragment(String itemTitle) {
+        System.out.println(itemTitle);
+        switch (itemTitle) {
+            case "Tasks": return this.tasksFragment;
+            case "Groups": return this.groupsFragment;
+            case "Friends": return this.friendsFragment;
+            case "Settings": return this.settingsFragment;
+        }
+        return null;
+    }
+
+    private void setFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayout,fragment).commit();
     }
 }
