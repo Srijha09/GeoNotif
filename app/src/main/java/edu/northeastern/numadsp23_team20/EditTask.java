4adb55cc6a7594f17fa630ef98859468106adccd
Rahul Suresh <rahs98@gmail.com>
2023-03-23 22:39:40 -0400

    Edit task layout

--- app/src/main/java/edu/northeastern/numadsp23_team20/EditTask.java
+++ app/src/main/java/edu/northeastern/numadsp23_team20/EditTask.java
@@ -5,6 +5,8 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,6 +15,8 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
@@ -31,21 +35,26 @@ import java.util.List;
 
 public class EditTask extends AppCompatActivity {
 
-    ActivityResultLauncher<Intent> addressSearchActivity;
-    MapView map;
+    private ActivityResultLauncher<Intent> addressSearchActivity;
+    private TextView editTaskLocationValue;
+    private MapView map;
+    private IMapController mapController;
+    private Marker mapMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_task);
 
+        this.editTaskLocationValue = findViewById(R.id.EditTaskLocationValue);
         // Places.initialize(getApplicationContext(), BuildConfig.MAPS_API_KEY);
         this.addressSearchActivity = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             result -> {
                 if (result.getResultCode() == AppCompatActivity.RESULT_OK) {
                     Place place = Autocomplete.getPlaceFromIntent(result.getData());
-                    System.out.println(place.getName() + ", " + place.getLatLng());
+                    setMapMarker(place.getLatLng().latitude, place.getLatLng().longitude);
+                    editTaskLocationValue.setText(place.getName());
                 }
             }
         );
@@ -53,7 +62,13 @@ public class EditTask extends AppCompatActivity {
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         this.map = findViewById(R.id.EditTaskMapView);
-        this.customizeMap();
+        this.mapController = this.map.getController();
+        this.mapMarker = new Marker(this.map);
+        double mapMarkerLatitude = 42.3447;
+        double mapMarkerLongitude = -71.0996;
+        this.configureMap();
+        this.customizeMapMarker();
+        this.setMapMarker(mapMarkerLatitude, mapMarkerLongitude);
     }
 
     public void onEditTaskUpdateButtonClick(View view) {
@@ -63,26 +78,44 @@ public class EditTask extends AppCompatActivity {
         this.addressSearchActivity.launch(intent);
     }
 
-    private void customizeMap() {
-        // Set map tiles
+    public void onEditTaskCancelButtonClick(View view) {
+        this.finish();
+    }
+
+    public void onEditTaskSubmitButtonClick(View view) {
+        new AlertDialog.Builder(this)
+            .setMessage("Are you sure you want to edit this task?")
+            .setPositiveButton("CONFIRM", (dialogInterface, whichButton) -> {
+                this.finish();
+            })
+            .setNegativeButton(android.R.string.no, null)
+            .show();
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void configureMap() {
         this.map.setTileSource(TileSourceFactory.MAPNIK);
-        IMapController mapController = this.map.getController();
-        mapController.setZoom(18.8);
-        // Set center
-        GeoPoint centerPoint = new GeoPoint(42.3447, -71.0996);
-        mapController.setCenter(centerPoint);
-        // Set marker
-        Marker marker = new Marker(this.map);
-        marker.setPosition(centerPoint);
-        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        this.mapController.setZoom(18.8);
+        this.map.setOnTouchListener((v, event) -> true);
+    }
+
+    private void customizeMapMarker() {
         Drawable d = ResourcesCompat.getDrawable(getResources(), R.drawable.pin, null);
         Bitmap bitmap = ((BitmapDrawable) d).getBitmap();
         Drawable dr = new BitmapDrawable(getResources(), Bitmap.createScaledBitmap(bitmap,
                 (int) (18.0f * getResources().getDisplayMetrics().density),
                 (int) (18.0f * getResources().getDisplayMetrics().density),
                 true));
-        marker.setIcon(dr);
-        // this.map.setOnTouchListener((v, event) -> true);
-        this.map.getOverlays().add(marker);
+        this.mapMarker.setIcon(dr);
+    }
+
+    private void setMapMarker(double latitude, double longitude) {
+        this.map.invalidate();
+        this.mapMarker.remove(this.map);
+        GeoPoint markerPoint = new GeoPoint(latitude, longitude);
+        this.mapController.setCenter(markerPoint);
+        this.mapMarker.setPosition(markerPoint);
+        this.mapMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        this.map.getOverlays().add(this.mapMarker);
     }
 }
