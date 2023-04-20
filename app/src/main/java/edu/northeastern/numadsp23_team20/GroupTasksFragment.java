dd2a3b8cf37aa7f25f565f3c849d8c7bc513bc35
Srijha09 <srijha.1999@gmail.com>
2023-04-19 20:47:01 -0400

    added GroupTaskRecyclerView

--- app/src/main/java/edu/northeastern/numadsp23_team20/GroupTasksFragment.java
+++ app/src/main/java/edu/northeastern/numadsp23_team20/GroupTasksFragment.java
@@ -24,6 +24,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import org.osmdroid.api.IMapController;
@@ -35,15 +37,21 @@ import org.osmdroid.views.overlay.Marker;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
-public class GroupTasksFragment extends Fragment implements GroupSettingsView.GroupNameChangedListener{
+public class GroupTasksFragment extends Fragment implements OnTaskItemClickListener{
 
     Context ctx;
     private MapView map;
     private IMapController mapController;
     private ActivityResultLauncher<Intent> addTaskActivityLaunch;
     private TaskService taskService;
-    private List<Task> taskList;
+    private List<Task> grouptaskList;
+    private boolean loadingTasks;
+    private ProgressBar tasksLoadingSpinner;
+    private TextView noTasksTextView;
+    private ScrollView tasksScrollView;
+    private GroupTasksAdapter taskListAdapter;
     private ImageButton settings;
     private String groupName;
 
@@ -66,6 +74,53 @@ public class GroupTasksFragment extends Fragment implements GroupSettingsView.Gr
         // Set the group name as the text of the TextView
         TextView groupNameTextView = inflatedView.findViewById(R.id.groupName);
         groupNameTextView.setText(groupName);
+
+        this.loadingTasks = true;
+        this.ctx = getContext();
+        Configuration.getInstance().load(this.ctx, PreferenceManager.getDefaultSharedPreferences(this.ctx));
+        this.map = inflatedView.findViewById(R.id.TasksMapView);
+        this.mapController = this.map.getController();
+        this.configureMap();
+        RecyclerView tasksRecyclerView = inflatedView.findViewById(R.id.TasksRecyclerView);
+        this.tasksLoadingSpinner = inflatedView.findViewById(R.id.TasksLoadingSpinner);
+        this.noTasksTextView = inflatedView.findViewById(R.id.NoTasksTextView);
+        this.tasksScrollView = inflatedView.findViewById(R.id.TasksScrollView);
+        this.grouptaskList = new ArrayList<>();
+        this.taskService = new TaskService();
+        LocationItem locationItem = new LocationItem("Fenway park", 42.3467, -71.0972);
+        Task task1 = new Task("Fenway test task", "Fenway test task description",
+                locationItem);
+        UUID taskUuid = UUID.randomUUID();
+        task1.setUuid(taskUuid.toString());
+        grouptaskList.add(task1);
+
+        this.taskListAdapter = new GroupTasksAdapter(this.grouptaskList, (OnTaskItemClickListener) this);
+        tasksRecyclerView.setAdapter(taskListAdapter);
+        tasksRecyclerView.setHasFixedSize(true);
+        tasksRecyclerView.setLayoutManager(new LinearLayoutManager(this.ctx));
+//        this.taskService.setTaskServiceListener(new TaskService.TaskServiceListener() {
+//            @SuppressLint("NotifyDataSetChanged")
+//            @Override
+//            public void onTaskLoaded(Task task) {
+//                if (task == null) {
+//                    loadingTasks = false;
+//                    tasksLoadingSpinner.setVisibility(View.INVISIBLE);
+//                    noTasksTextView.setVisibility(View.VISIBLE);
+//                    tasksScrollView.setVisibility(View.INVISIBLE);
+//                    return;
+//                }
+//                if (loadingTasks) {
+//                    loadingTasks = false;
+//                    tasksLoadingSpinner.setVisibility(View.INVISIBLE);
+//                    noTasksTextView.setVisibility(View.INVISIBLE);
+//                    tasksScrollView.setVisibility(View.VISIBLE);
+//                }
+//                //setMapMarker(task);
+//                grouptaskList.add(task);
+//                taskListAdapter.notifyDataSetChanged();
+//            }
+//        });
+
         settings = inflatedView.findViewById(R.id.settings_button);
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,16 +137,29 @@ public class GroupTasksFragment extends Fragment implements GroupSettingsView.Gr
         return inflatedView;
     }
 
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (context instanceof GroupSettingsView) {
-            ((GroupSettingsView) context).setGroupNameChangedListener(this);
-        }
+    @SuppressLint("ClickableViewAccessibility")
+    private void configureMap() {
+        this.map.setTileSource(TileSourceFactory.MAPNIK);
+        this.mapController.setZoom(15);
+        this.map.setMultiTouchControls(true);
+        this.map.setClickable(true);
     }
 
+
     @Override
-    public void onGroupNameChanged(String newGroupName) {
-        this.groupName = newGroupName;
+    public void onTaskItemClick(int position) {
+//        Task task = this.grouptaskList.get(position);
+//        Intent intent = new Intent(getContext(), TaskView.class);
+//        intent.putExtra("position", position);
+//        intent.putExtra("taskTitle", task.getTaskName());
+//        intent.putExtra("taskDescription", task.getDescription());
+//        intent.putExtra("taskLocation", task.getLocation().getKey());
+//        intent.putExtra("taskLatitude", task.getLocation().getLat());
+//        intent.putExtra("taskLongitude", task.getLocation().getLon());
+//        intent.putExtra("taskComplete", task.getIsComplete());
+//        intent.putExtra("taskUUID", task.getUuid());
+//        intent.putExtra("taskType", task.getTaskType());
+//        intent.putExtra("taskTypeString", task.getTaskTypeString());
+//        this.viewTaskActivityLaunch.launch(intent);
     }
 }
